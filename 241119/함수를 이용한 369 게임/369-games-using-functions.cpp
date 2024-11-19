3a094115b7147d907112240585f54b8496463a49
migi0401 <104820707+migi0401@users.noreply.github.com>
2024-11-19 15:13:43 +0900

    Title: 함수를 이용한 369 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241119/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+bool JudgeNum(int m) {
+    while (m > 0) {
+        if (m % 10 == 3 || m % 10 == 6 || m % 10 == 9)
+            return true;
+        m = m / 10;
+    }
+}
+
+bool FindNum(int n) {
+    return n % 3 == 0 || JudgeNum(n);
+}
+
+int main() {
+    int num = 0;;
+    int a, b;
+    cin >> a >> b;
+    for (int i = a; i <= b; i++) {
+        if (FindNum(i)) {
+            num++;
+        }
+    }
+    cout << num;
+    return 0;
+}
